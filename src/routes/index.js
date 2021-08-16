98fde1b2af3b28557b7cd6539d1aef92b7c68895
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2021-08-16 20:44:16 -0300

    Limpando o index.html e adicionando nova pagina e nova rota ao projeto..

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from '../views/Home';
 import Cadastro from '../views/Cadastro';
+import Calculadora from '../views/Calculadora';
 
 export default function Routes() {
   return (
@@ -10,6 +11,7 @@ export default function Routes() {
       <Route path="/" exact component={Home} />
       <Route path="/cadastro" component={Cadastro} />
       <Route path="/editar/:id" component={Cadastro} />
+      <Route path="/calculadora" component={Calculadora} />
     </Switch>
   );
 }
