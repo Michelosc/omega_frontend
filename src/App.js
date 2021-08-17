34fc5ae78a0673d3d55f5ed832d5e9fe993ce223
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2021-08-16 21:25:41 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Routes from './routes';
+import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <Routes />
-      <ToastContainer />
-    </>
+    </BrowserRouter>
   );
 }
