2470349f3077f3c4290e99101f6083032116ed84
Arnaldo Silva <rsnowsdev@gmail.com>
2021-08-19 18:06:21 -0300

    Adicionada tela de cadastro com funcionalidade basica

--- src/views/Cadastro/index.js
+++ src/views/Cadastro/index.js
@@ -1,12 +1,68 @@
-import React from 'react';
-import './style.css';
+import React, { useState } from 'react'
+import axios from 'axios';
+import logo from '../../assets/cadastro.jpg'
+import omega from '../../assets/logo-omega-energia.png'
+import './style.css'
 
-export default function Cadastro() {
+
+const Cadastro = () => {
+
+  const url = "";
+
+  const [data, setData] = useState({
+    nome: "",
+    email: "",
+    senha: ""
+  })
+
+  function submit(e){
+    e.preventDefault();
+    axios.post(url, {
+      nome: data.nome,
+      email: data.email,
+      senha: data.senha
+    })
+    .then(res =>{
+      console.log(res.data)
+    })
+  }
+
+  function handle(e){
+    const cadastrar={...data}
+    cadastrar[e.target.id] = e.target.value;
+    setData(cadastrar);
+    console.log(cadastrar);
+  }
+
   return (
-    <>
-      <div>
-        <h1>Pagina Cadastro</h1>
+    <div className="cadastro">
+      <div className="cadastro-img-left" >
+        <img src={logo} alt="imagem de página de cadastro" />
       </div>
-    </>
-  );
+
+      <div className="cadastro-direita">
+        <img src={omega} alt="logo-omega-energia" />
+
+        <form onSubmit={(e) => submit(e)}>
+          <div className="CadastroNome">
+            <label>Nome</label>
+           <input type="text" id="nome" placeholder="Digite seu nome" value={data.nome} onChange={e => handle(e)}/>
+          </div>
+
+          <div className="CadastroEmail">
+            <label>Email</label>
+            <input type="email" id="email" placeholder="Digite seu email" value={data.email} onChange={e => handle(e)}/>
+          </div>
+
+          <div className="CadastroSenha">
+            <label>Senha</label>
+            <input type="password" id="senha" placeholder="Digite sua senha" value={data.senha} onChange={e => handle(e)}/>
+          </div>
+
+          <button className="btn-cadastrar">Cadastrar</button>
+        </form>
+      </div>
+    </div>
+  )
 }
+export default Cadastro;
