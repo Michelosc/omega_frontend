aaa58a0835890f5228fe1a8be5a80abeeb79bada
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2021-08-20 22:48:56 -0300

    ...

--- src/views/Calculadora/index.js
+++ src/views/Calculadora/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import logo from '../../assets/omega-logo.png';
 import './style.css';
 
 export default function Calculadora() {
   return (
-    <div class="box">
-      <div class="card">
-        <h1>ola</h1>
+    <div className="box">
+      <div className="card">
+        <div className="container-header">
+          <img className="imagem-header" src={logo} alt="Logo Uol" />
+          <h6>Olá cliente</h6>
+        </div>
+        <div className="container-body">
+          <div className="colOne">ola teste um</div>
+          <div className="colTwo">ola teste dois</div>
+        </div>
       </div>
     </div>
   );
